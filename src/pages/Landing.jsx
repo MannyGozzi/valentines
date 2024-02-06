8d1fe5a1577dd5c08d93bb3546cbe516a3035821
Noctua <mannygozzidev@protonmail.com>
2024-02-05 22:38:45 -0800

    Finished more or lessg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,71 @@
-function Landing() {
-  return (
-    <div className='flex justify-center items-center p-8'>
-        <h1 className="font-bold">Be my Valentine!</h1>
-        <div className="mt-12" id="wrapper">
-            <div id="pulsingheart"></div>
-        </div>
+import { useState } from 'react'
+import { IoCube } from "react-icons/io5";
+import { FaLink } from "react-icons/fa";
 
-    </div>
+
+
+function Landing() {
+
+// eslint-disable-next-line no-undef
+const [toName, setToName] = useState('');
+const [fromName, setFromName] = useState('');
+const inputStyle = `border-pink-500 bg-pink-300 
+rounded-2xl p-2 border-[.3rem] mb-4 placeholder-pink-500 text-pink-500 font-bold text-2xl text-center`
+const anims = `!outline-none focus:ring-8 focus:ring-pink-400 focus:bg-pink-200 hover:ring-8 hover:ring-pink-400 hover:bg-pink-200 transition-all duration-300 ease-in-out`
+const [link, setLink] = useState("")
+
+const genLink = () => {
+    const url = new URL(window.location.href);
+    url.pathname = "/valentine";
+    url.searchParams.set('to', toName);
+    url.searchParams.set('from', fromName);
+    setLink(url.href);
+}
+
+  return (
+    <div className='flex flex-col justify-center items-center p-8 h-[100vh]'>
+        <h1 className="font-bold text-pink-500 rounded-2xl bg-pink-100 p-4">Be my Valentine!</h1>
+        
+        <div className="flex flex-col items-center mt-8">
+            <label className="text-lg font-semibold mb-2 text-black" htmlFor="toName">
+                To:
+            </label>
+            <input
+                type="text"
+                id="toName"
+                value={toName}
+                onChange={(e) => setToName(e.target.value)}
+                placeholder="Recipient's Name"
+                className={`${inputStyle} ${anims}`}
+            />
+
+            <label className="text-lg font-semibold mb-2 text-black" htmlFor="fromName">
+                From:
+            </label>
+            <input
+                type="text"
+                id="fromName"
+                value={fromName}
+                onChange={(e) => setFromName(e.target.value)}
+                placeholder="Your Name"
+                className={`${inputStyle} ${anims}`}
+            />
+            <button
+                type="button"
+                className={`${anims} bg-pink-300 border-[.3rem] border-pink-500 text-pink-500 text-2xl font-bold rounded-2xl p-2 mt-4 mb-4 letter-spacing-2 flex items-center justify-center gap-3`}
+                onClick={genLink}
+            ><p className='h-full'>Build</p><IoCube className='transition-all duration-500 hover:text-black hover:rotate-90'/> 
+            </button>
+            </div>
+
+            {
+                link && (
+                    <a href={link} target="_blank" rel="noreferrer" className={`transition-all duration-300 ${link.length > 40 ? "text-sm" : "text-lg"} p-4 bg-pink-100 rounded-2xl flex  gap-2 text-pink-500 font-bold text-2xl mt-4`}>
+                        <FaLink />{link}
+                    </a>
+                )
+            }
+        </div>
   )
 }
 
--- src/pages/Valentine.jsx
+++ src/pages/Valentine.jsx
@@ -1,12 +1,70 @@
+import {useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const anims = `!outline-none focus:ring-8 focus:ring-pink-400 focus:bg-pink-200 hover:ring-8 hover:ring-pink-400 hover:bg-pink-200 transition-all duration-300 ease-in-out`
+const buttonStyles = "bg-pink-300 border-[.3rem] border-pink-500 text-pink-500 text-2xl font-bold rounded-2xl p-4 mt-4 mb-4 letter-spacing-2 flex items-center justify-center gap-3 select-none"
 
 
 function Valentine() {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  let toName = params.get('to')
+  let fromName = params.get('from')
+  if (!toName) toName = "Valentine";
+  if (!fromName) fromName = "Secret Admirer";
+  const [no, setNo] = useState(0);
+  const gifs = ["kissing.gif", "belly.gif", "love_you.gif", "spinning.gif", "fall.gif", "carry.gif"]
+  const [shout, setShout] = useState(`${toName}, will you be my Valentine?`);
+  const handleClick = () => {
+    const noCount = no + 1
+    if (no < 4) setNo(prevCount => prevCount + 1);
+    if (noCount == 1) {
+      setShout("But I love you!");
+    } else if (noCount == 2) {
+      setShout("I really do!");
+    } else if (noCount == 3) {
+      setShout("Please?");
+    } else if (noCount == 4) {
+      setShout("Are you serious! 😭");
+    }
+  };
+
+  const yesClick = () => {
+    setNo(gifs.length-1)
+    setShout(`Yayyyyy! I love you!`);
+  }
+
   return (
-    <div>
-      <h1>Valentines</h1>
-      <p>Valentines content</p>
+    <div 
+    className="flex flex-col h-screen justify-center items-center">
+      <div className="mt-12" id="wrapper">
+            <div id="pulsingheart" className="scale-[200%] mb-[4rem]"></div>
+        </div>
+      <h2 className="text-2xl text-pink-500 font-bold mb-4">From: {fromName}</h2>
+      <div className='flex justify-center mb-4'><img
+          src={gifs[no]}  // Make sure to adjust the path based on your project structure
+          alt="Valentine GIF"
+          className="rounded-2xl shadow-lg w-[300px] h-[300px] transition-all duration-300 hover:scale-110 object-cover	"
+        />
+        </div>
+      <div className={`flex transition-all duration-300 justify-center item-center gap-6  ${no >= 4 ? "max-h-0 opacity-0 overflow-hidden" :"max-h-[300px]"}`}>
+        <button
+            type="button"
+            className={`${anims} ${buttonStyles}`}
+            onClick={yesClick}
+        ><p className='h-full'>Yes</p>
+        </button>
+        <button
+            type="button"
+            className={`${anims} ${buttonStyles}`}
+            style={{ transform: `scale(${100 - no * 15}%)` }}
+            onClick={handleClick}
+        ><p className='h-full'>No</p>
+        </button>
+      </div>
+      <h2 className="text-2xl text-pink-500 font-bold mt-4">{shout}</h2>
     </div>
   );
-}
+    }
 
 export default Valentine
