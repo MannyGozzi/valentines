9fa79888763898c8a2c3d5400f46896e7fd2abc9
Noctua <mannygozzidev@protonmail.com>
2024-02-05 20:26:03 -0800

    starter code

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <div className="">
+        <h1 className='bg-red-300'>Valentines</h1>
+
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/pages/Valentine.jsx
@@ -0,0 +1,12 @@
+
+
+function Valentine() {
+  return (
+    <div>
+      <h1>Valentines</h1>
+      <p>Valentines content</p>
+    </div>
+  );
+}
+
+export default Valentine
